5d65b7bc6ae9aba49299115e3b768c6eebbbfad4
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-24 22:33:10 +0100

    added:expressjs//displayed json data

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var http= require('http');
+/*var http= require('http');
 var url= require('url');
 
 var queryString=require('querystring');
@@ -10,11 +10,34 @@ var httpServer =http.createServer(function (req,resp) {
    query=url.parse(req.url).query;
    console.log("query:"+query);
    params=queryString.parse(query);
-   //console.log("-params-:"+params);
    console.log("--params[name]--:"+ params['name']);
 });
 
 const port= (process.env.PORT||'8899');
 httpServer.listen(port,function () {
     console.log("Server start at :"+port);
-})
+})*/
+var express=require('express');
+const {json} = require("express");
+const process = require("process");
+var app=express();
+
+app.get('/',(req, res)=>{
+   res.setHeader("content-type","text/html");
+   res.send("<h1>Hello Express js developper</h1>");
+});
+
+app.get('/books/:code',(req, res)=>{
+   res.setHeader("content-type","application/json");
+   var infos={
+      name:"Bonevy BEBY",
+      email:"bonevybeby@formationkilo.com",
+      code:req.params.code
+   }
+   res.send(JSON.stringify(infos));
+});
+
+const port=(process.env.PORT||'8988');
+app.listen(port,()=>{
+   console.log("Server started at port: "+port);
+});
