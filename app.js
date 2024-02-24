5043b8bf5599c3faa3b784eb808b213fe7d3485c
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-24 22:02:17 +0100

    retrieve data request from url

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var http= require('http');
 var url= require('url');
 
+var queryString=require('querystring');
+
 var httpServer =http.createServer(function (req,resp) {
    resp.writeHead(200,{'content-type':'text/html'});
-   resp.end("<h1>Test node js with the server</h1>") ;
+   resp.end("<h1>Test node js with the server</h1>");
    console.log(url.parse(req.url).path);
+   query=url.parse(req.url).query;
+   console.log("query:"+query);
+   params=queryString.parse(query);
+   //console.log("-params-:"+params);
+   console.log("--params[name]--:"+ params['name']);
 });
 
 const port= (process.env.PORT||'8899');
