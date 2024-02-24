b5df9e9fd75463489fa4f545e5a4ae7b769ca3f5
Bonevy BEBY <bonevybeby@gmail.com>
2024-02-24 21:29:34 +0100

    retrieve url in node.js pure code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var http= require('http');
-
+var url= require('url');
 
 var httpServer =http.createServer(function (req,resp) {
-
    resp.writeHead(200,{'content-type':'text/html'});
    resp.end("<h1>Test node js with the server</h1>") ;
+   console.log(url.parse(req.url).path);
 });
 
 const port= (process.env.PORT||'8899');
